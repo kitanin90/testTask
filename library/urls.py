013f6034716ef22c5af9cfed59dc25f66371cfbe
kitanin90 <kitanin90@mail.ru>
2019-11-23 03:27:34 +0500

    Add CRUD all model

--- library/urls.py
+++ library/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from .views import AssignmentView, BookView, UserView
+from .views import *
 
 app_name = "assignments"
 
 urlpatterns = [
     # assignments
     path('assignments/', AssignmentView.as_view()),
-    path('assignments/<int:pk>', AssignmentView.as_view()),
+    path('assignments/<int:pk>', SingleAssignmentView.as_view()),
 
     #books
     path('books/', BookView.as_view()),
-    path('books/<int:pk>', BookView.as_view()),
+    path('books/<int:pk>', SingleBookView.as_view()),
 
     #users
     path('users/', UserView.as_view()),
-    path('users/<int:pk>', UserView.as_view()),
+    path('users/<int:pk>', SingleUserView.as_view()),
 ]
--- library/views.py
+++ library/views.py
@@ -1,50 +1,48 @@
-from rest_framework.generics import get_object_or_404
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.generics import get_object_or_404, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Assignment, Book, User
 from .serializers import UserSerializer, BookSerializer, AssignmentSerializer
 
 
-class AssignmentView(ListModelMixin, CreateModelMixin, GenericAPIView):
+# Assignment
+class AssignmentView(ListCreateAPIView):
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         user = get_object_or_404(User, id=self.request.data.get('user'))
         book = get_object_or_404(Book, id=self.request.data.get('book'))
         return serializer.save(user=user, book=book)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+
+class SingleAssignmentView(RetrieveUpdateDestroyAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
 
 
-class BookView(ListModelMixin, CreateModelMixin, GenericAPIView):
+# Book
+class BookView(ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
     def perform_create(self, serializer):
         return serializer.save()
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+
+class SingleBookView(RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
-class UserView(ListModelMixin, CreateModelMixin, GenericAPIView):
+# User
+class UserView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
     def perform_create(self, serializer):
         return serializer.save()
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+
+class SingleUserView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
